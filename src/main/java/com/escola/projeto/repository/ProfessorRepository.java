ba66abaf8791ad3d02fd2059a26632cabaab2070
Luizzzin <luizhenribarbodias@gmail.com>
2025-04-12 11:37:12 -0300

    feat: professorrepository

--- src/main/java/com/escola/projeto/repository/ProfessorRepository.java
+++ src/main/java/com/escola/projeto/repository/ProfessorRepository.java
@@ -1,4 +1,7 @@
 package com.escola.projeto.repository;
 
-public interface ProfessorRepository {
+import com.escola.projeto.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 }
