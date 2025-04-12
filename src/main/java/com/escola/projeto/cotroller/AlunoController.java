88c94034e72724d2182bb3790204ecd9b0e15662
Luizzzin <luizhenribarbodias@gmail.com>
2025-04-12 15:42:05 -0300

    feat:up

--- src/main/java/com/escola/projeto/cotroller/AlunoController.java
+++ src/main/java/com/escola/projeto/cotroller/AlunoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,7 +25,24 @@ public class AlunoController {
         List<Aluno> alunos = alunoService.listarTodos();
         model.addAttribute("alunos", alunos);
         return ("alunos/lista");
-
     }
+
+    //criando formulario para novo aluno(Create)
+    @GetMapping("/novo")
+    public String salvarAlunos(@ModelAttribute Aluno aluno){
+        alunoService.salvarAluno(aluno);
+        return "redirect:/alunos";
+    }
+
+    //editar aluno (Edit)
+    @GetMapping("/editar/{id}")
+    public String editarAlunoForm(@PathVariable Long id, Model model){
+        Aluno aluno = alunoService.buscarPorId(id)
+            .orElseThrow(() ->new RuntimeException("Aluno não encontrado com o ID" + id));
+    model.addAttribute("aluno",aluno);
+        return "aluno/form";
+    }
+
+
 
 }
